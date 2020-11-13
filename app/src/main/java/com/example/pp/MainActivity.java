bf4f524426144533f8a51a6f2bd519d832341fc1
aau31andres <58749123+aau31andres@users.noreply.github.com>
2020-11-13 20:08:10 +0100

    este es el bueno bueno 2.0 (final)

--- app/src/main/java/com/example/pp/MainActivity.java
+++ app/src/main/java/com/example/pp/MainActivity.java
@@ -1,31 +1,34 @@
 package com.example.pp;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.loader.app.LoaderManager;
-import androidx.loader.content.Loader;
+
 
 import android.Manifest;
-import android.content.ContentResolver;
+
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.net.Uri;
-import android.os.Build;
+
 import android.os.Bundle;
+
 import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
+
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+
 import android.widget.ListView;
+
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.TwoLineListItem;
 
 import java.util.ArrayList;
-import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
 
@@ -36,7 +39,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     ArrayList<String> numerList = new ArrayList<>();
     ArrayList<String> numerList2 = new ArrayList<>();
     ArrayList String;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,8 +57,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             pedirPermisos();
         }
 
+        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" ASC");
 
-        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, R.layout.mi_layout, R.id.txtNombre, mobileArray);
+
+
+        SimpleCursorAdapter adaptador = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, phones, PROJECTION2, new int[]{android.R.id.text1});
 
         lista.setAdapter(adaptador);
 
@@ -91,57 +96,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
 
-    /*private ArrayList nombreContactos() {
-        ArrayList<String> nameList = new ArrayList<>();
-        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, , null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
-        while (phones.moveToNext()) {
 
-            String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-
-
-            nameList.add(name);
-
-
-        }
-        phones.close();
-
-
-        return nameList;
-    }*/
-
-
-    /*private ArrayList getNumbers() {
-        ArrayList<String> nameList = new ArrayList<>();
-        ContentResolver cr = getContentResolver();
-        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
-                null, null, null, null);
-        if ((cur != null ? cur.getCount() : 0) > 0) {
-            while (cur != null && cur.moveToNext()) {
-                String id = cur.getString(
-                        cur.getColumnIndex(ContactsContract.Contacts._ID));
-                String name = cur.getString(cur.getColumnIndex(
-                        ContactsContract.Contacts.DISPLAY_NAME));
-                nameList.add(name);
-                if (cur.getInt(cur.getColumnIndex( ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
-                    Cursor pCur = cr.query(
-                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                            null,
-                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
-                            new String[]{id}, null);
-                    while (pCur.moveToNext()) {
-                        String phoneNo = pCur.getString(pCur.getColumnIndex(
-                                ContactsContract.CommonDataKinds.Phone.NUMBER));
-
-                    }
-                    pCur.close();
-                }
-            }
-        }
-        if (cur != null) {
-            cur.close();
-        }
-        return nameList;
-    }*/
 
 
     public void pedirPermisos() {
@@ -169,7 +124,14 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         finish();
     }
 
+    private static final String[] PROJECTION2 = new String[]{
 
+
+
+            ContactsContract.Contacts.DISPLAY_NAME,
+            ContactsContract.Contacts._ID
+
+    };
 
     private static final String[] PROJECTION = new String[]{
 
@@ -191,28 +153,19 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
             Intent datosContactos = new Intent(this, DatosContacto.class);
 
-
-            String name = parent.getItemAtPosition(position).toString();
+            String name = java.lang.String.valueOf((((TextView)view).getText()));
 
             String selection = "DISPLAY_NAME='"+ name +"'";
 
             datosContactos.putExtra("nombre", name);
 
-
-        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, PROJECTION, selection, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" ASC");
-        //String phoneNumber = phones.getString(Integer.parseInt(phones.toString()));
-
+            Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, PROJECTION, selection, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" ASC");
 
             phones.moveToFirst();
             String n = phones.getString(1);
 
 
-
-
-            //String numero = numerList2.get(position);
-
             datosContactos.putExtra("numero",n);
-
 
             startActivity(datosContactos);
 
@@ -225,76 +178,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
 
 
-
-
-
-        /*datosContactos.putExtra("nombre", name);
-        datosContactos.putExtra("numero", pho);
-
-        startActivity(datosContactos);
-
-
-        finish();*/
-
-
-
-
-
-
-
-
-
-        /*Map<String, String> namePhoneMap = new HashMap<String, String>();
-
-        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
-
-        // Loop Through All The Numbers
-        while (phones.moveToNext()) {
-
-            String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-            String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-
-            // Cleanup the phone number
-            phoneNumber = phoneNumber.replaceAll("[()\\s-]+", "");
-
-            // Enter Into Hash Map
-            namePhoneMap.put(phoneNumber, name);
-
-        }
-
-        // Get The Contents of Hash Map in Log
-        for (Map.Entry<String, String> entry : namePhoneMap.entrySet()) {
-            String key = entry.getKey();
-            Toast.makeText(this, key, Toast.LENGTH_SHORT).show();
-            String value = entry.getValue();
-            Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-        }
-
-        phones.close();*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*ArrayList<String> numberlista = new ArrayList<>();
-        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" ASC");
-        Toast.makeText(this, "Nombre: " + nombre, Toast.LENGTH_SHORT).show();
-        String name = parent.getItemAtPosition(position).toString();
-        //String numero= phones.get((ContactsContract.CommonDataKinds.Phone.NUMBER)).toString();
-        //String numero = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));*/
-        //String name2 = (String) parent.getItemAtPosition(Integer.parseInt(ContactsContract.CommonDataKinds.Phone.NUMBER.toString()));
 
 
 
